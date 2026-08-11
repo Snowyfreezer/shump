3f11126a18ca189b8434162e79bdd01815e44c4e
Snowyfreezer <99560297+Snowyfreezer@users.noreply.github.com>
2026-08-11 04:16:34 -0400

    holding focus button (shift) makes you slow down

--- Player/player.gd
+++ Player/player.gd
@@ -10,23 +10,33 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	# if not is_on_floor():
 	# 	velocity += get_gravity() * delta
-
 	# Handle jump.
 	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	# 	velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX := Input.get_axis("left", "right")
 	if directionX:
-		velocity.x = directionX * speed
+		if Input.is_action_pressed("focus"):
+			velocity.x = directionX * focusSpeed
+		else:
+			velocity.x = directionX * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		if Input.is_action_pressed("focus"):
+			velocity.x = move_toward(velocity.x, 0, focusSpeed)
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
 
 	var directionY := Input.get_axis("up", "down")
 	if directionY:
-		velocity.y = directionY * speed
+		if Input.is_action_pressed("focus"):
+			velocity.y = directionY * focusSpeed
+		else:			
+			velocity.y = directionY * speed
 	else:
-		velocity.y = move_toward(velocity.y, 0, speed)
-
+		if Input.is_action_pressed("focus"):
+			velocity.y = move_toward(velocity.y, 0, focusSpeed)
+		else:
+			velocity.y = move_toward(velocity.y, 0, speed)
+			
 	move_and_slide()

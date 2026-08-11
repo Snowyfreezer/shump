0075e514349c49fa0894aadcca9aa49b01f8dee1
Snowyfreezer <99560297+Snowyfreezer@users.noreply.github.com>
2026-08-11 04:17:52 -0400

    cleaned a little comments

--- Player/player.gd
+++ Player/player.gd
@@ -3,16 +3,11 @@ extends CharacterBody2D
 
 @export var speed: float = 300.0
 @export var focusSpeed: float = 150.0
-# const JUMP_VELOCITY = -400.0
 
+# TODO: make focus circle visible when holding shift
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# if not is_on_floor():
-	# 	velocity += get_gravity() * delta
-	# Handle jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX := Input.get_axis("left", "right")
@@ -38,5 +33,5 @@ func _physics_process(delta: float) -> void:
 			velocity.y = move_toward(velocity.y, 0, focusSpeed)
 		else:
 			velocity.y = move_toward(velocity.y, 0, speed)
-			
+
 	move_and_slide()
